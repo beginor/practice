c54f2316d5b72b98057bd8e991b3bef168a82c40
beginor <beginor@qq.com>
2012-01-13 17:59:01 +0800

    add tasks test

---
+++ test/silverlight/SilverlightUnitTest/TasksTest.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.Silverlight.Testing;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SilverlightUnitTest {
+
+	[TestClass]
+	public class TasksTest : SilverlightTest {
+		
+		[TestMethod]
+		public void TestCreateTaskFromAsync() {
+			Assert.IsTrue(true);
+		}
+
+		[TestMethod]
+		public void TestFromCallback() {
+			Assert.Fail();
+		}
+	}
+}
