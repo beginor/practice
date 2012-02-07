cf1979d04e67217fae7d3ab82cc4f38bd6d83345
beginor <beginor@qq.com>
2012-02-07 21:07:42 +0800

    add layer delegate

---
+++ src/monotouch/CAStudy/LayerDelegate.cs
@@ -0,0 +1,18 @@
+using System;
+using MonoTouch.CoreAnimation;
+using MonoTouch.Foundation;
+
+namespace CAStudy {
+
+	public class LayerDelegate : CALayerDelegate {
+		
+		public override void DisplayLayer(CALayer layer) {
+			
+		}
+		
+		public override void DrawLayer(CALayer layer, MonoTouch.CoreGraphics.CGContext context) {
+			
+		}
+	}
+}
+
