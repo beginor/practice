e2fd4197b849d8b446cf5113b5f5b6e16b785294
beginor <beginor@qq.com>
2012-03-17 22:26:45 +0800

    add put post delete method

--- src/server/HttpWebApp/Controllers/ProductsController.cs
+++ src/server/HttpWebApp/Controllers/ProductsController.cs
@@ -1,7 +1,9 @@
-﻿using System.Linq;
+﻿using System;
+using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
 using System.Web.Http;
 using HttpWebApp.Models;
 
@@ -9,30 +11,55 @@ namespace HttpWebApp.Controllers {
 
 	public class ProductsController : HibernateApiController<NorthwindContext> {
 
-		public ProductsController(NorthwindContext context) : base(context) {
+		public ProductsController(NorthwindContext context)
+			: base(context) {
 		}
 
-		public IQueryable<Product> GetAll() {
+		public IQueryable<Product> GetAllProducts() {
 			return this.HibernateContext.Products;
 		}
 
-		public HttpResponseMessage<Product> GetById(int id) {
+		public Product GetProductById(int id) {
 			var product = this.HibernateContext.Products.FirstOrDefault(p => p.ProductID == id);
-			
-			var formatters = new MediaTypeFormatter[] {
-				new XmlMediaTypeFormatter(), 
-				new JsonMediaTypeFormatter()
-			};
 
 			if (product == null) {
-				throw new HttpResponseException("not  found your id.", HttpStatusCode.NotFound);
+				throw new HttpResponseException(HttpStatusCode.NotFound);
 			}
-			
-			return new HttpResponseMessage<Product>(product, formatters);
+
+			return product;
 		}
 
-		public HttpResponseMessage<Product> Post(int id, Product product) {
-			throw new HttpResponseException("Not implemented", HttpStatusCode.NotImplemented);
+		public HttpResponseMessage<Product> PostProduct(Product product) {
+			using (var trans = this.HibernateContext.Session.BeginTransaction()) {
+				this.HibernateContext.Session.Update(product);
+				trans.Commit();
+			}
+			var result = new HttpResponseMessage<Product>(product, HttpStatusCode.Created);
+			var location = Url.Route(null, new {
+				id = product.ProductID
+			});
+			result.Headers.Location = new Uri(location);
+			return result;
+		}
+
+		public HttpResponseMessage PutProduct(int id, Product product) {
+			using (var trans = this.HibernateContext.Session.BeginTransaction()) {
+				product.ProductID = id;
+				this.HibernateContext.Session.Save(product);
+				trans.Commit();
+			}
+			return new HttpResponseMessage(HttpStatusCode.OK);
+		}
+
+		public HttpResponseMessage DeleteProduct(int id) {
+			var product = this.HibernateContext.Products.FirstOrDefault(p => p.ProductID == id);
+			if (product == null) {
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+			}
+			var trans = this.HibernateContext.Session.BeginTransaction();
+			this.HibernateContext.Session.Delete(product);
+			trans.Commit();
+			return new HttpResponseMessage(HttpStatusCode.NoContent);
 		}
 	}
 }
