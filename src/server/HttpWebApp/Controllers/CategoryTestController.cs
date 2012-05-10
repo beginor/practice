a41d7bf20d1262eb433e39fa1b525cf662d580b4
beginor <beginor@qq.com>
2012-05-10 21:36:05 +0800

    add post test, not success.

--- src/server/HttpWebApp/Controllers/CategoryTestController.cs
+++ src/server/HttpWebApp/Controllers/CategoryTestController.cs
@@ -1,12 +1,17 @@
-﻿using System.Net.Http;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Web.Http;
+using System.Web.Routing;
 using HttpWebApp.Models;
 
 namespace HttpWebApp.Controllers {
 
 	public class CategoryTestController : ApiController {
 		
-		private static Category[] _data = new [] {
+		private static readonly IList<Category> Data = new List<Category> {
 			new Category {
 				CategoryID = 1,
 				CategoryName = "Cat 1",
@@ -24,8 +29,29 @@ namespace HttpWebApp.Controllers {
 			}
 		};
 
-		public HttpResponseMessage<Category[]> GetAllCategories() {
-			return new HttpResponseMessage<Category[]>(_data);
+		public IQueryable<Category> GetAllCategories() {
+			return Data.AsQueryable();
 		}
+
+		public Category GetCategoryById(int id) {
+			var category = Data.FirstOrDefault(c => c.CategoryID == id);
+			if (category == null) {
+				throw new HttpResponseException(HttpStatusCode.NotFound);
+			}
+			return category;
+		}
+
+		public HttpResponseMessage<Category> PostCategory(Category category) {
+			var categoryId = Data.Max(c => c.CategoryID) + 1;
+			category.CategoryID = categoryId;
+			Data.Add(category);
+
+			var message = new HttpResponseMessage<Category>(category, HttpStatusCode.Created);
+			var url = Url.Route(null, new {
+				id = categoryId
+			});
+			message.Headers.Location = new Uri(url);
+			return message;
+		} 
 	}
 }
--- test/desktop/UnitTest/TestHttpClient.cs
+++ test/desktop/UnitTest/TestHttpClient.cs
@@ -1,22 +1,30 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Net.Http.Headers;
 using HttpWebApp.Models;
 using NUnit.Framework;
 using System.Net.Http;
+using System.Net.Http.Formatting;
 
 namespace UnitTest {
 
 	[TestFixture]
 	public class TestHttpClient {
 
-		[Test]
-		public void TestRequest() {
-			var client = new HttpClient {
-				BaseAddress = new Uri("http://localhost:25422/api/", UriKind.Absolute)
+		private HttpClient _httpClient;
+
+		[SetUp]
+		public void SetUp() {
+			this._httpClient = new HttpClient {
+				BaseAddress = new Uri("http://localhost:25422/api/categorytest/", UriKind.Absolute)
 			};
-			var requestTask = client.GetAsync("categorytest");
+		}
+
+		[Test]
+		public void TestGetAll() {
+			var requestTask = this._httpClient.GetAsync("");
 			requestTask.Wait();
 			var responseMessage = requestTask.Result;
 			responseMessage.EnsureSuccessStatusCode();
@@ -27,6 +35,37 @@ namespace UnitTest {
 			var result = readTask.Result;
 
 			Assert.IsTrue(result.Any());
+		}
+
+		[Test]
+		public void TestGetById() {
+			const int id = 1;
+			var requestTask = this._httpClient.GetAsync(id.ToString(CultureInfo.InvariantCulture));
+			requestTask.Wait();
+			var responseMsg = requestTask.Result;
+			responseMsg.EnsureSuccessStatusCode();
+
+			var readTask = responseMsg.Content.ReadAsAsync<Category>();
+			readTask.Wait();
+
+			var result = readTask.Result;
+
+			Assert.AreEqual(id, result.CategoryID);
+		}
+
+		[Test]
+		public void TestPost() {
+			var cat = new Category {
+				CategoryName = "My category",
+				Description = "My category description"
+			};
+			var formatter = new JsonMediaTypeFormatter();
+			var requestMsg = new HttpRequestMessage<Category>(cat, new MediaTypeHeaderValue("application/json"), new [] { formatter } );
+			
+			var requestTask = this._httpClient.PostAsync("", requestMsg.Content);
+			requestTask.Wait();
+			var responseMessage = requestTask.Result;
+			responseMessage.EnsureSuccessStatusCode();
 		}
 	}
 }
