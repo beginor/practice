17862b0c617c119b1e2073a8665102595ed91ef1
beginor <beginor@qq.com>
2012-05-12 17:30:04 +0800

    add logger implement

---
+++ src/server/HttpWebApp/Logger.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace HttpWebApp {
+
+	public class Logger : System.Web.Http.Common.ILogger {
+
+		private log4net.ILog _log = log4net.LogManager.GetLogger(typeof(Logger));
+
+		public void LogException(string category, TraceLevel level, Exception exception) {
+			_log.DebugFormat("category: {0}, level: {1}, exception: {2}", category, level, exception);
+		}
+
+		public void Log(string category, TraceLevel level, Func<string> messageCallback) {
+			_log.DebugFormat("category: {0}, level: {1}, message: {2}", category, level, messageCallback());
+		}
+	}
+}
