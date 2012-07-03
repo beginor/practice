1535f3b284a670095830314b9121e431b57385d0
beginor <beginor@qq.com>
2012-07-03 12:45:50 +0800

    Thread Async Start

---
+++ dotnet/ThreadSync/Form1.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace ThreadSync {
+
+	public partial class Form1 : Form {
+
+		public Form1() {
+			InitializeComponent();
+		}
+
+		private void button1_Click(object sender, EventArgs e) {
+
+		}
+	}
+}
