9ac95ab2ff8dbaf9b81bf8718c2ad1bcac795a75
beginor <beginor@qq.com>
2012-07-03 13:38:10 +0800

    form create new SynchronizationContext for current thread automaticly

--- dotnet/ThreadSync/Program.cs
+++ dotnet/ThreadSync/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Threading;
 using System.Windows.Forms;
 
 namespace ThreadSync {
@@ -10,9 +11,29 @@ namespace ThreadSync {
 		/// </summary>
 		[STAThread]
 		static void Main() {
+			Console.WriteLine("0.ThreadID:#{1},Synchronization Context is null?{0}", SynchronizationContext.Current == null, Thread.CurrentThread.ManagedThreadId);
+
 			Application.EnableVisualStyles();
 			Application.SetCompatibleTextRenderingDefault(false);
-			Application.Run(new Form1());
+
+			Test a = new Test();
+			Console.WriteLine("1.ThreadID:#{1},Synchronization Context is null?{0}", SynchronizationContext.Current == null, Thread.CurrentThread.ManagedThreadId);
+
+			var form = new Form1();
+			Console.WriteLine("2.ThreadID:#{1},Synchronization Context is null?{0}", SynchronizationContext.Current == null, Thread.CurrentThread.ManagedThreadId);
+			//Application.Run(form);
+
+			new Thread(Work).Start();
+
+			Console.Read();
 		}
+
+		static void Work() {
+			Console.WriteLine("3.ThreadID:#{1},Synchronization Context is null?{0}",
+			  SynchronizationContext.Current == null, Thread.CurrentThread.ManagedThreadId);
+		}
+	}
+
+	public class Test {
 	}
 }
