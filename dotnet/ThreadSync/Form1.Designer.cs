219e96bea865b7b6470537a188baceaa8985fc21
beginor <beginor@qq.com>
2012-07-03 12:58:48 +0800

    Cross-thread operation not valid:

--- dotnet/ThreadSync/Form1.Designer.cs
+++ dotnet/ThreadSync/Form1.Designer.cs
@@ -24,6 +24,7 @@
 		/// </summary>
 		private void InitializeComponent() {
 			this.button1 = new System.Windows.Forms.Button();
+			this.label1 = new System.Windows.Forms.Label();
 			this.SuspendLayout();
 			// 
 			// button1
@@ -36,11 +37,20 @@
 			this.button1.UseVisualStyleBackColor = true;
 			this.button1.Click += new System.EventHandler(this.button1_Click);
 			// 
+			// label1
+			// 
+			this.label1.Location = new System.Drawing.Point(13, 56);
+			this.label1.Name = "label1";
+			this.label1.Size = new System.Drawing.Size(259, 29);
+			this.label1.TabIndex = 1;
+			this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+			// 
 			// Form1
 			// 
 			this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
 			this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
 			this.ClientSize = new System.Drawing.Size(284, 262);
+			this.Controls.Add(this.label1);
 			this.Controls.Add(this.button1);
 			this.Name = "Form1";
 			this.Text = "Form1";
@@ -51,6 +61,7 @@
 		#endregion
 
 		private System.Windows.Forms.Button button1;
+		private System.Windows.Forms.Label label1;
 	}
 }
 
