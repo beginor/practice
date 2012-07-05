3c494d89eaa15b2030607ee9522894d6d1941abd
beginor <beginor@qq.com>
2012-07-05 20:59:12 +0800

    ex22 completed.

--- dotnet/TaskExceptions/Program.cs
+++ dotnet/TaskExceptions/Program.cs
@@ -3,53 +3,62 @@ using System.Threading;
 using System.Threading.Tasks;
 
 
-namespace TaskExceptions {
+namespace TaskExceptions
+{
 
-	class Program {
+    class Program
+    {
 
-		static void Main(string[] args) {
+        static void Main(string[] args)
+        {
 
-			TaskScheduler.UnobservedTaskException += (s, e) => {
-				Console.WriteLine(e.Exception);
-			};
+            TaskScheduler.UnobservedTaskException += (s, e) =>
+            {
+                //e.SetObserved();
+                Console.WriteLine(e.Exception);
+            };
 
-			TestAsync(5, -10);
+            TestAsync(5, -10);
 
-			//testTask.ContinueWith(task => {
-			//   if (task.IsFaulted) {
-			//      Console.WriteLine(task.Exception.GetBaseException());
-			//   }
-			//   else {
-			//      Console.WriteLine(task.Result);
-			//   }
-			//});
+            //testTask.ContinueWith(task => {
+            //   if (task.IsFaulted) {
+            //      Console.WriteLine(task.Exception.GetBaseException());
+            //   }
+            //   else {
+            //      Console.WriteLine(task.Result);
+            //   }
+            //});
 
-			//try {
-			//   t.Wait();
-			//}
-			//catch(Exception ex) {
-			//   Console.WriteLine(ex);
-			//}
+            //try {
+            //   t.Wait();
+            //}
+            //catch(Exception ex) {
+            //   Console.WriteLine(ex);
+            //}
 
-			Thread.Sleep(TimeSpan.FromMilliseconds(3000));
+            Thread.Sleep(TimeSpan.FromMilliseconds(3000));
 
-			GC.Collect();
+            GC.Collect();
 
-			Console.WriteLine("Completed.");
-			//Console.ReadLine();
-		}
+            Console.WriteLine("Completed.");
+            //Console.ReadLine();
+        }
 
-		static Task<int> TestAsync(int a, int b) {
-			var tcs = new TaskCompletionSource<int>();
-			Task.Factory.StartNew(() => {
-				if (a + b < 0) {
-					tcs.TrySetException(new InvalidOperationException("a + b < 0"));
-				}
-				else {
-					tcs.TrySetResult(a + b);
-				}
-			});
-			return tcs.Task;
-		}
-	}
+        static Task<int> TestAsync(int a, int b)
+        {
+            var tcs = new TaskCompletionSource<int>();
+            Task.Factory.StartNew(() =>
+            {
+                if (a + b < 0)
+                {
+                    tcs.TrySetException(new InvalidOperationException("a + b < 0"));
+                }
+                else
+                {
+                    tcs.TrySetResult(a + b);
+                }
+            });
+            return tcs.Task;
+        }
+    }
 }
