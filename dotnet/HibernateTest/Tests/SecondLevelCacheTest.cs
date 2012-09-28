1c8d8179fde5f477fef7b4bf85bbae1462913275
beginor <beginor@qq.com>
2012-09-28 15:33:55 +0800

    enable second leven cache in config, add a test file

---
+++ dotnet/HibernateTest/Tests/SecondLevelCacheTest.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Linq;
+using HibernateTest.Models;
+using NHibernate.Linq;
+using NUnit.Framework;
+using NHibernate;
+using NHibernate.Cfg;
+
+namespace HibernateTest.Tests {
+
+	[TestFixture]
+	public class SecondLevelCacheTest {
+
+		private ISessionFactory _sessionFactory;
+
+		[TestFixtureSetUp]
+		public void SetUp() {
+			if (this._sessionFactory == null) {
+				var cfg = new Configuration();
+				cfg.Configure();
+				this._sessionFactory = cfg.BuildSessionFactory();
+			}
+		}
+
+		[Test]
+		public void TestConfig() {
+			Assert.IsNotNull(this._sessionFactory);
+		}
+
+		[Test]
+		public void TestLambdaQuery() {
+			
+		}
+	}
+}
