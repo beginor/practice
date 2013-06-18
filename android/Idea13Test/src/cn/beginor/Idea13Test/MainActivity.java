8c16d090cf9bec7ba2199180b3a35ab1cb867874
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2013-06-18 22:07:11 +0800

    add idea 13 test project

---
+++ android/Idea13Test/src/cn/beginor/Idea13Test/MainActivity.java
@@ -0,0 +1,15 @@
+package cn.beginor.Idea13Test;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MainActivity extends Activity {
+    /**
+     * Called when the activity is first created.
+     */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
