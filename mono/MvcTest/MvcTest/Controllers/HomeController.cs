38f61cf07c83df7096ed5ce5978b4e5b4c3a3751
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-01-15 08:36:21 +0800

    try run asp.net mvc5 + unity + ef6 on mono

---
+++ mono/MvcTest/MvcTest/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System.Web.Mvc;
+using System.Linq;
+
+namespace MvcTest.Controllers {
+
+    public class HomeController : Controller {
+
+        public string Message {
+            get;
+            set;
+        }
+
+        public NorthwindContext Database {
+            get;
+            set;
+        }
+
+        public ActionResult Index() {
+            ViewBag.Title = "Home Page";
+            ViewBag.Message = Message;
+            return View();
+        }
+
+        [Route("~/home/test")]
+        public ActionResult AttrRouteTest() {
+            return this.Content("Hello, returned by action attribute routing.");
+        }
+
+        [Route("~/home/cat")]
+        public ActionResult Categories() {
+            var data = Database.Categories.ToList();
+            return this.View(data);
+        }
+    }
+}
---
+++ mono/MvcTest/MvcTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,35 @@
+﻿using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+// General Information about an assembly is controlled through the following 
+// set of attributes. Change these attribute values to modify the information
+// associated with an assembly.
+[assembly: AssemblyTitle("MvcWebApp")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MvcWebApp")]
+[assembly: AssemblyCopyright("Copyright ©  2013")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+// Setting ComVisible to false makes the types in this assembly not visible 
+// to COM components.  If you need to access a type in this assembly from 
+// COM, set the ComVisible attribute to true on that type.
+[assembly: ComVisible(false)]
+
+// The following GUID is for the ID of the typelib if this project is exposed to COM
+[assembly: Guid("035a06cd-b766-4b4b-9a44-9d6bcf2d916e")]
+
+// Version information for an assembly consists of the following four values:
+//
+//      Major Version
+//      Minor Version 
+//      Build Number
+//      Revision
+//
+// You can specify all the values or you can default the Revision and Build Numbers 
+// by using the '*' as shown below:
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
