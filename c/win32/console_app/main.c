d48b72399019946ef0ca0f0bc057e8d3879110a4
beginor <beginor@qq.com>
2014-03-06 14:03:07 +0800

    Concatenating strings

--- c/win32/console_app/main.c
+++ c/win32/console_app/main.c
@@ -2,11 +2,20 @@
 #include <wchar.h>
 
 int wmain(int argc, WCHAR *argv[]) {
-    char *name = "Jane";
-    wchar_t *town = L"Bratislava";
+    wchar_t *s1 = L"Zetcode, ";
+    wchar_t *s2 = L"tutorials ";
+    wchar_t *s3 = L"for ";
+    wchar_t *s4 = L"programmers.\n";
 
-    wprintf(L"The length of the name string is %d\n", lstrlenA(name));
-    wprintf(L"The town string length is %d\n", lstrlenW(town));
+    int len = lstrlenW(s1) + lstrlenW(s2) + lstrlenW(s3) + lstrlenW(s4);
+    wchar_t *buff = malloc(sizeof(wchar_t) * (len + 1));
+    lstrcpyW(buff, s1);
+    lstrcatW(buff, s2);
+    lstrcatW(buff, s3);
+    lstrcatW(buff, s4);
 
+    wprintf(buff);
+
+    free(buff);
     return EXIT_SUCCESS;
 }
