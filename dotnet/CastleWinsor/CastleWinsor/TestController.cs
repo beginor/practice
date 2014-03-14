1e2946200e2596f9cc331672f4c7d59231bcb4de
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-03-14 16:30:30 +0800

    play with Castle Windsor

---
+++ dotnet/CastleWinsor/CastleWinsor/TestController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace CastleWinsor {
+
+    public class TestController {
+
+        readonly ILog log;
+
+        public string Database {
+            get;
+            set;
+        }
+
+        public TestController(ILog log) {
+            this.log = log;
+        }
+
+        public void DoAction() {
+            log.Log(string.Format("Do action with database: {0}", Database));
+        }
+    }
+}
+
