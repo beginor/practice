27db31db990384beaef5b4ae7af6dee098ca33ac
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-06-11 13:11:40 +0800

    candy search droid init check in

---
+++ android/CandySearch/MainActivity.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+using Android.App;
+using Android.Content;
+using Android.Runtime;
+using Android.Views;
+using Android.Widget;
+using Android.OS;
+
+namespace CandySearch {
+
+    [Activity(Label = "CandySearch", MainLauncher = true)]
+    public class MainActivity : Activity {
+
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
+
+            SetContentView(Resource.Layout.Main);
+
+        }
+    }
+}
