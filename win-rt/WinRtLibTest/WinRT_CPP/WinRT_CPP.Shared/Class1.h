7b3e28a87099fc3d8e091cdf615a52b993adbb67
beginor <beginor@qq.com>
2014-06-13 07:12:00 +0800

    add win-rt test project.

---
+++ win-rt/WinRtLibTest/WinRT_CPP/WinRT_CPP.Shared/Class1.h
@@ -0,0 +1,27 @@
+﻿#pragma once
+
+#include <collection.h>
+#include <amp.h>
+#include <amp_math.h>
+
+using namespace Windows::Foundation;
+using namespace Windows::Foundation::Collections;
+
+namespace WinRT_CPP {
+
+    public delegate void PrimeFoundHandler(int result);
+
+    public ref class Class1 sealed {
+
+    public:
+        Class1();
+        // Synchronous method. 
+        IVector<double>^  ComputeResult(double input);
+
+        // Asynchronous methods
+        IAsyncOperationWithProgress<IVector<int>^, double>^ GetPrimesOrdered(int first, int last);
+        IAsyncActionWithProgress<double>^ GetPrimesUnordered(int first, int last);
+
+        event PrimeFoundHandler^ primeFoundEvent;
+    };
+}
