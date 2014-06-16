1b23d74353bf5c4ba59ddcc5b7a362de08c76b26
beginor <beginor@qq.com>
2014-06-17 06:47:17 +0800

    weight improvment

--- c/SortingAlgorithms/SortingAlgorithms/QuickUnion.c
+++ c/SortingAlgorithms/SortingAlgorithms/QuickUnion.c
@@ -12,8 +12,11 @@
 QuickUnion * QuickUnion_create(const int N) {
     QuickUnion * result = malloc(sizeof(QuickUnion *));
     result->N = N;
+    result->arr = malloc(N * sizeof(int));
+    result->sz = malloc(N * sizeof(int));
     for (int i = 0; i < N; i++) {
         result->arr[i] = i;
+        result->sz = 1;
     }
     return result;
 }
@@ -32,5 +35,15 @@ int QuickUnion_connected(QuickUnion * qu, const int p, const int q) {
 void QuickUnion_union(QuickUnion * qu, const int p, const int q) {
     int i = QuickUnion_root(qu, p);
     int j = QuickUnion_root(qu, q);
-    qu->arr[i] = j;
+    if (i == j) {
+        return;
+    }
+    if (qu->arr[i] < qu->arr[j]) {
+        qu->arr[i] = j;
+        qu->sz[j] += qu->sz[i];
+    }
+    else {
+        qu->arr[j] = i;
+        qu->zs[i] += qu->sz[j];
+    }
 }
--- c/SortingAlgorithms/SortingAlgorithms/QuickUnion.h
+++ c/SortingAlgorithms/SortingAlgorithms/QuickUnion.h
@@ -14,6 +14,7 @@
 typedef struct {
     
     int * arr;
+    int * sz;
     int N;
     
 } QuickUnion;
