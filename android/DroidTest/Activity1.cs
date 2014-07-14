bf6af318629e31af24d5640cd74b173ea6372561
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-07-14 14:02:36 +0800

    test application class

--- android/DroidTest/Activity1.cs
+++ android/DroidTest/Activity1.cs
@@ -1,41 +1,35 @@
 using System;
-
 using Android.App;
 using Android.Content;
 using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
-using TouchTest;
 
 namespace DroidTest {
 
-	[Activity (Label = "DroidTest", MainLauncher = true)]
-	public class Activity1 : Activity {
-		int count = 1;
+    [Activity(Label = "DroidTest", MainLauncher = true)]
+    public class Activity1 : Activity {
 
-		protected override void OnCreate(Bundle bundle) {
-			base.OnCreate(bundle);
+        int count = 1;
 
-			// Set our view from the "main" layout resource
-			SetContentView(Resource.Layout.Main);
+        protected override void OnCreate(Bundle savedInstanceState) {
+            base.OnCreate(savedInstanceState);
 
-			// Get our button from the layout resource,
-			// and attach an event to it
-			Button button = FindViewById<Button>(Resource.Id.myButton);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.Main);
+
+            // Get our button from the layout resource,
+            // and attach an event to it
+            Button button = FindViewById<Button>(Resource.Id.myButton);
 			
-			button.Click += delegate {
-				button.Text = string.Format("{0} clicks!", count++);
-			};
+            button.Click += delegate {
+                button.Text = string.Format("{0} clicks!", count++);
+            };
 
-			var cat = new Cat { Name = "Tom" };
-			var cat2 = new Cat { Name = "cat" };
-			cat.MakeFriends(cat2);
-			var dog = new Dog { Name = "Tigger" };
-			dog.MakeFriends(cat);
-			dog.MakeFriends(cat2);
-		}
-	}
+			
+        }
+    }
 }
 
 
---
+++ android/DroidTest/DroidApp.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using Android.Runtime;
+
+namespace DroidTest {
+
+    [Android.App.Application]
+    public class DroidApp : Android.App.Application {
+
+        public DroidApp(IntPtr javaRef, JniHandleOwnership transfer) : base(javaRef, transfer) {
+            Android.Util.Log.Info("DroidApp", "DroidApp Constructor");
+        }
+
+        public override void OnCreate() {
+            Android.Util.Log.Info("DroidApp", "DroidApp OnCreate");
+            base.OnCreate();
+        }
+    }
+}
+
