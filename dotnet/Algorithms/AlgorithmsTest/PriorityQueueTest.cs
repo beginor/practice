4b883e1d7cad41389910bdb429183e9475584913
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-07-24 14:05:39 +0800

    pq test.

---
+++ dotnet/Algorithms/AlgorithmsTest/PriorityQueueTest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using NUnit.Framework;
+using Algorithms;
+
+namespace AlgorithmsTest {
+
+    [TestFixture]
+    public class PriorityQueueTest {
+    
+        [Test]
+        public void TestMinPQ() {
+            var arr = CreateRandomArray(10);
+            Insertion<int>.Sort(arr);
+            StdOut.WriteLine(arr);
+
+            var pq = new MinPQ<int>();
+            foreach (int i in arr) {
+                pq.Insert(i);
+            }
+
+            while (!pq.IsEmpty) {
+                StdOut.WriteLine("delete min: {0}", pq.DelMin());
+            }
+        }
+
+        [Test]
+        public void TestMaxPQ() {
+            var arr = CreateRandomArray(10);
+            Insertion<int>.Sort(arr);
+            StdOut.WriteLine(arr);
+
+            var pq = new MaxPQ<int>();
+            foreach (int i in arr) {
+                pq.Insert(i);
+            }
+
+            while (!pq.IsEmpty) {
+                StdOut.WriteLine("delete max: {0}", pq.DelMax());
+            }
+        }
+
+        private static int[] CreateRandomArray(int n) {
+            var a = new int[n];
+            for (int i = 0; i < a.Length; i++) {
+                a[i] = StdRandom.Uniform(100);
+            }
+            return a;
+        }
+    }
+}
+
