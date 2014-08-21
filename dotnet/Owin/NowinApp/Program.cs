95c99fca67d3d42d749b77af5cc3254199b593d0
beginor <beginor@qq.com>
2014-08-22 07:46:59 +0800

    owin sample

---
+++ dotnet/Owin/NowinApp/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Nowin;
+using Microsoft.Owin.Builder;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+namespace NowinApp {
+
+    class Program {
+
+        public static void Main(string[] args) {
+            var appBuilder = new AppBuilder();
+            Nowin.OwinServerFactory.Initialize(appBuilder.Properties);
+
+            var startup = new WebApiOwin.Startup();
+            startup.Configuration(appBuilder);
+
+            var builder = new ServerBuilder();
+            builder.SetAddress(System.Net.IPAddress.Parse("127.0.0.1")).SetPort(8888)
+                   .SetOwinApp(appBuilder.Build())
+                   .SetOwinCapabilities((IDictionary<string, object>)appBuilder.Properties[OwinKeys.ServerCapabilitiesKey]);
+            //Console.WriteLine("Hello World!");
+            using (var server = builder.Build()) {
+
+                Task.Run(() => server.Start());
+
+                Console.WriteLine("Listening 127.0.0.1:8888");
+                Console.ReadLine();
+            }
+        }
+    }
+}
---
+++ dotnet/Owin/WebApiOwin/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Owin;
+using System.Web.Http;
+using Microsoft.Owin.Hosting;
+using System.Net.Http;
+
+namespace WebApiOwin {
+
+    class Program {
+
+        public static void Main(string[] args) {
+            var baseAddress = "http://localhost:9000/";
+            using (WebApp.Start<Startup>(baseAddress)) {
+                // Create HttpCient and make a request to api/values 
+                HttpClient client = new HttpClient(); 
+
+                var response = client.GetAsync(baseAddress + "api/values").Result; 
+
+                Console.WriteLine(response); 
+                Console.WriteLine(response.Content.ReadAsStringAsync().Result);
+
+                Console.ReadLine();
+            }
+            Console.ReadLine();
+        }
+    }
+
+    public class Startup {
+
+        public void Configuration(IAppBuilder appBuilder) {
+            var config = new HttpConfiguration();
+            config.MapHttpAttributeRoutes();
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+            appBuilder.UseWebApi(config);
+        }
+    }
+}
