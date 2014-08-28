b3ef455d563dcd6eceaa8c3b959e1ecbc9f2371a
beginor <beginor@qq.com>
2014-08-28 08:00:07 +0800

    owin01 hello world

---
+++ dotnet/Owin/Owin01_Helloworld/Startup.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using Owin;
+using Microsoft.Owin;
+using Microsoft.Owin.Hosting;
+using System.Threading.Tasks;
+
+namespace Owin01_Helloworld {
+
+    public class Startup {
+
+        public void Configuration(IAppBuilder appBuilder) {
+            appBuilder.Run(HandleRequest);
+        }
+
+        Task HandleRequest(IOwinContext context) {
+            context.Response.ContentType = "text/plain";
+            return context.Response.WriteAsync("Hello, world!");
+        }
+    }
+}
+
