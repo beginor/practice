9e8ac5a8b12c755deb8ad9e3358bb8344bb074e9
beginor <beginor@qq.com>
2014-09-01 07:46:59 +0800

    update packages

--- dotnet/Owin/Owin02_WebApi/Controllers/ValuesController.cs
+++ dotnet/Owin/Owin02_WebApi/Controllers/ValuesController.cs
@@ -7,25 +7,25 @@ namespace Owin02_WebApi.Controllers {
     public class ValuesController : ApiController {
 
         // GET api/values
-        public IEnumerable<string> Get() { 
+        public IEnumerable<string> Get() {
             return new string[] { "value1", "value2" }; 
         }
 
         // GET api/values/5
-        public string Get(int id) { 
+        public string Get(int id) {
             return "value"; 
         }
 
         // POST api/values
-        public void Post([FromBody]string value) { 
+        public void Post([FromBody]string value) {
         }
 
         // PUT api/values/5
-        public void Put(int id, [FromBody]string value) { 
+        public void Put(int id, [FromBody]string value) {
         }
 
         // DELETE api/values/5
-        public void Delete(int id) { 
+        public void Delete(int id) {
         }
     }
 }
