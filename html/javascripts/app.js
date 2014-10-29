88e89fde6bb3bc7b87cecb4ccca77ab9baa047b7
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-10-29 11:14:42 +0800

    rename to AngularJS to html, rewrite css with compass, fix navigation layout.

---
+++ html/javascripts/app.js
@@ -0,0 +1,13 @@
+﻿'use strict';
+
+var demo = angular.module('demo', ['ngRoute', 'demoControllers']);
+
+demo.config([
+    '$routeProvider',
+    function ($rootProvider) {
+        $rootProvider
+            .when('/dialogs', { templateUrl: 'views/dialogs.html', controller: 'DialogsController' })
+            .when('/list', { templateUrl: 'views/list.html', controller: 'ListController' })
+            .otherwise({ redirectTo: '/dialogs' });
+    }
+]);
---
+++ html/javascripts/controllers.js
@@ -0,0 +1,11 @@
+﻿'use strict';
+
+var demoControllers = angular.module('demoControllers', []);
+
+demoControllers.controller('DialogsController', ['$scope', function ($scope) {
+    $scope.greeting = 'Hello';
+}]);
+
+demoControllers.controller('ListController', ['$scope', function($scope) {
+    $scope.greeting = 'List';
+}]);
