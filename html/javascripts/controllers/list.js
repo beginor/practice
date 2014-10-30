578bae38b11bf89624ed30dd91e3edf23a8ac40c
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-10-30 19:37:46 +0800

    more views

---
+++ html/javascripts/controllers/list.js
@@ -0,0 +1,17 @@
+﻿(function(angular) {
+    'use strict';
+
+    var list = angular.module('list', []);
+
+    list.controller('ListController', ['$scope',
+        //
+        function ($scope) {
+            $scope.greeting = 'List controller';
+
+            //
+            $scope.$on('$destroy', function(evt) {
+                console.log('list controller destroy.');
+            });
+        }
+    ]);
+}(window.angular));
