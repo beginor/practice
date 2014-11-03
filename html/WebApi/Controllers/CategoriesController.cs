7c607ce1cfca483962c25141cbde085e84daa095
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-11-03 11:51:33 +0800

    category controller refactor

--- html/WebApi/Controllers/CategoriesController.cs
+++ html/WebApi/Controllers/CategoriesController.cs
@@ -11,18 +11,22 @@ namespace WebApi.Controllers {
     /// <summary>
     /// Summary description for CategoryController
     /// </summary>
-    public class CategoryController : ApiController {
+    public class CategoriesController : ApiController {
 
-        private static readonly IList<Category> Data = new List<Category> {
-            new Category { CategoryId = 1, CategoryName = "Beverages", Description = "Soft drinks, coffees, teas, be"},
-            new Category { CategoryId = 2, CategoryName = "Condiments", Description = "Sweet and savory sauces, relis"},
-            new Category { CategoryId = 3, CategoryName = "Confections", Description = "Desserts, candies, and sweet b"},
-            new Category { CategoryId = 4, CategoryName = "Dairy Products", Description = "Cheeses"},
-            new Category { CategoryId = 5, CategoryName = "Grains/Cereals", Description = "Breads, crackers, pasta, and c"},
-            new Category { CategoryId = 6, CategoryName = "Meat/Poultry", Description = "Prepared meats"},
-            new Category { CategoryId = 7, CategoryName = "Produce", Description = "Dried fruit and bean curd"},
-            new Category { CategoryId = 8, CategoryName = "Seafood", Description = "Seaweed and fish"}
-        };
+        private static readonly IList<Category> Data;
+
+        static CategoriesController() {
+            Data = new List<Category> {
+                new Category { CategoryId = 1, CategoryName = "Beverages", Description = "Soft drinks, coffees, teas, be"},
+                new Category { CategoryId = 2, CategoryName = "Condiments", Description = "Sweet and savory sauces, relis"},
+                new Category { CategoryId = 3, CategoryName = "Confections", Description = "Desserts, candies, and sweet b"},
+                new Category { CategoryId = 4, CategoryName = "Dairy Products", Description = "Cheeses"},
+                new Category { CategoryId = 5, CategoryName = "Grains/Cereals", Description = "Breads, crackers, pasta, and c"},
+                new Category { CategoryId = 6, CategoryName = "Meat/Poultry", Description = "Prepared meats"},
+                new Category { CategoryId = 7, CategoryName = "Produce", Description = "Dried fruit and bean curd"},
+                new Category { CategoryId = 8, CategoryName = "Seafood", Description = "Seaweed and fish"}
+            };
+        }
 
         public IEnumerable<Category> GetAll() {
             return Data;
