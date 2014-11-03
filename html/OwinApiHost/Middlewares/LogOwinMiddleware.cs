fafc69ed5af78ddec82add8e918ca879c46f5d6c
zhangzhimin <zhangzhimin@gdepb.gov.cn>
2014-11-03 11:52:03 +0800

    show response status code

--- html/OwinApiHost/Middlewares/LogOwinMiddleware.cs
+++ html/OwinApiHost/Middlewares/LogOwinMiddleware.cs
@@ -13,6 +13,7 @@ namespace OwinApiHost.Middlewares {
         public async override Task Invoke(IOwinContext context) {
             Console.WriteLine("{0} {1} {2}", DateTime.Now, context.Request.Method, context.Request.Uri);
             await Next.Invoke(context);
+            Console.WriteLine("Return HTTP status: {0}", context.Response.StatusCode);
         }
 
     }
